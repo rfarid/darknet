16429d6550c7c96fa8fb8fe9d9ed9bf196723f3f
Reza Farid <reza.farid@roames.com.au>
2016-04-18 15:47:18 +1000

    adding pole and misc to labels/make_labels.py, modifying run_test, adjusting yolo_kernels.cu for two classes

--- run_test.sh
+++ run_test.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
-if [ $# -lt 2 ]
+# Reza Farid, Fugro Roames
+# Created:      2016/04/15
+# Last update:  2016/04/18
+#
+# a shell script to test YOLO test with multiple files in a folder
+#
+if [ $# -lt 4 ]
 then
 	echo
 	echo
-	echo "USAGE: $(basename $0) folder files_pattern"
-	echo
-	echo
+	echo "USAGE: $(basename $0) config_file weights_file folder files_pattern"
+	echo "	config_file such as cfg/yolo_2.cfg"
+	echo "	weights_file suuch as ../rezaf/roames/YOLO/backup/yolo_2_40000.weights"
+	echo	
 	exit
 fi
-in_folder=$1
-files_pattern=$2
+config_file=$1
+weights_file=$2
+in_folder=$3
+files_pattern=$4
 i=1
 all_files=$(find $in_folder -name "$files_pattern")
 n=$(find $in_folder -name "$files_pattern" | wc -l)
 # echo $all_files,$n
-for in_file in $all_files; do # Not recommended, will break on whitespace
+for in_file in $all_files; do
     echo $in_file","$i" of "$n
-    ./darknet yolo test cfg/yolo_2.cfg ~/projects/rezaf/roames/YOLO/backup/yolo_2_40000.weights $in_file
+    ./darknet yolo test $config_file $weights_file $in_file
     let i=i+1
 done
 #find /home/rezaf/projects/rezaf/roames/testJPG/out -name "*.jpg" -exec ./darknet yolo test cfg/yolo_2.cfg ~/projects/rezaf/roames/YOLO/backup/yolo_2_40000.weights {} \;
